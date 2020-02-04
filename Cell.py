1f5a85e98744627d7a5c0a06d2da25df176ecc7f
Yogesh Patel <yogeshpatel@nbp-201-7.nbp.ruw.rutgers.edu>
2020-02-04 15:50:33 -0500

    cell object made

---
+++ Cell.py
@@ -0,0 +1,15 @@
+class Cell:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.isBlocked = False
+        self.onFire = False
+
+
+    def setBlockStatus(self, status):
+        self.isBlocked = status
+
+    def setFireStatus(self, status):
+        self.onFire = status
+        
